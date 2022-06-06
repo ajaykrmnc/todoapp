c53d7adc8ec5833c37e623ad6c83a23038b913b0
ajaykrmnc <ajaykg6917@gmail.com>
2022-06-06 17:57:21 +0530

    final Project
    Now the todolist is completed and Enjoy the todo list with backend

--- src/App.js
+++ src/App.js
@@ -18,6 +18,14 @@ class App extends Component {
       this.setState({todos})
 
     }
+    addTodo=(todo) =>
+    {
+      todo.id=Math.random();
+      let todos=[...this.state.todos,todo];
+      this.setState({todos});
+  
+
+    }
   
   
   render() { 
@@ -25,7 +33,7 @@ class App extends Component {
       <div className='todo-app container'>
         <h1 className='center blue-text'>Todo's </h1>
         <Todos todos ={this.state.todos} deleteTodo= {this.deleteTodo} />
-        < AddTodo />
+        < AddTodo addTodo={this.addTodo}/>
 
       </div>
     );
